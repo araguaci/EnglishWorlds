595ad6d9b19377f9cb20b90baf05a5477fae4450
Salim Djerbouh <tbitw31@gmail.com>
2016-10-06 16:41:14 +0200

    Navbar logged out

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -17,47 +17,62 @@ if (!isset($_SESSION["user_login"])) {
     <link rel="stylesheet" href="./css/master.css" media="screen">
     <link rel="stylesheet" href="./css/blue.css" media="screen">
     <link rel="stylesheet" href="./css/main.css" media="screen">
+    <link rel="stylesheet" href="./css/bootstrap.min.css" media="screen" title="no title">
     <script src="js/jquery.min.js"></script>
     <script src="js/jquery-color.js"></script>
     <script src="js/script.js"></script>
     <script src="js/placeholder-js.js" type="text/javascript"></script>
     <script src="js/main.js" type="text/javascript"></script>
+    <script src="./js/bootstrap.min.js"></script>
   </head>
   <body>
-    <div class="mashmenu">
-      <div id="menuWrapper">
-        <div class="fnav">
-          <a href="index.php" class="flink">Dz English</a>
-          <div class="allContent">
-            <div class="snav">
-              <a href="./about/index.html" class="slink">About</a>
-              <div class="insideContent">
-                <span class="featured">What is it?<br><a href="https://www.facebook.com/groups/IAMALGERIANANDISPEAKENGLISH/">Group</a>
-                </span>
-              </div>
-              <!-- End inside content. -->
-            </div>
-            <!-- End snav -->
-            <div class="snav">
-              <a href="#" class="slink">Open Source?</a>
-              <div class="insideContent">
-                <span class="featured"></span>
-              </div>
-              <!-- End inside content. -->
-            </div>
-            <!-- End snav -->
-            <div class="snav">
-              <a href="https://www.facebook.com/Saly3301" class="slink">Developer account</a>
-              <div class="insideContent">
-                <span class="featured"></span>
-              </div>
-              <!-- end snav -->
-            </div>
-          </div>
-          <!-- End All content. -->
-        </div>
-        <!-- End fnav -->
+    <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">English Dz</a>
+      </div>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <!-- set class="active" to the current active list item -->
+          <li><a href="login.php">Login<span class="sr-only"></span></a></li>
+          <li><a href="./about/index.html">About</a></li>
+          <!-- <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">Separated link</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">One more separated link</a></li>
+            </ul>
+          </li> -->
+        </ul>
         <?php
+        if (!$username) {
+          echo '<ul class="nav navbar-nav navbar-right">
+            <li><a href="signup.php">Sign Up</a></li>
+            </ul>';
+        } else {
+          echo '<a href="#">Profile</a>
+          <a href="#">Settings</a>';
+        }
+         ?>
+
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+  </nav>
+        <?php
+        /*
         if (!$username) {
           echo '<div class="fnav">
             <a href="index.php" class="flink">Sign Up</a>
@@ -80,43 +95,22 @@ if (!isset($_SESSION["user_login"])) {
           </div>
           <!-- end fnav -->';
         }
-
+        */
         ?>
-        <!-- <div class="feat">
-          <form id="searchForm">
-            <fieldset>
-              <div class="input">
-                <input type="text" class="Search" id="s" value="Search Dz English...">
-              </div>
-              <input type="submit" id="searchSubmit" value="">
-            </fieldset>
-          </form>
-        </div> -->
       </div>
     </div>
     <div class="headerMenu">
       <div id="wrapper">
-        <div class="logo">
-          <img src="./img/find_friends_logo.png" alt="Find Friends logo" />
-        </div>
-        <div class="search_box">
-          <form action="search.php" method="GET" id="search">
-            <input type="text" name="q" value="" size="60" placeholder="Search ...">
-          </form>
-        </div>
-        <div id="menu">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <?php
-          if (!$username) {
-            echo '<a href="#">Sign Up</a>
-            <a href="#">Sign In</a>';
-          } else {
-            echo '<a href="#">Profile</a>
-            <a href="#">Settings</a>';
-          }
-           ?>
-         </div><!-- end fnav feat -->
+
+        <?php if ($username){
+          echo '<div class="search_box">
+            <form action="search.php" method="GET" id="search">
+              <input type="text" name="q" value="" size="60" placeholder="Search ...">
+            </form>
+          </div>';
+        } else {
+          echo "";
+        } ?>
       </div>
     </div>
     <br>
--- js/script.js
+++
@@ -1,8 +0,0 @@
-$("document").ready(function(){
-  $("#menuWrapper").menu({
-    position: {
-      flink: "center top",
-      slink: "center bottom"
-    }
-  });
-});
