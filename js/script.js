648c954df26d309abba8676f69ad546af6eaddc7
Salim Djerbouh <tbitw31@gmail.com>
2016-10-06 18:10:10 +0200

    alert empty username and password

--- js/script.js
+++ js/script.js
@@ -5,7 +5,16 @@ function validateLoginForm()
       if (username==null || username=="",password==null || password=="")
         {
           // Set bootstrap warning.
-        alert("Please check all fields");
-        return false;
-        }
+          var div = document.createElement("div");
+          div.className = "alert alert-danger";
+          div.role = "alert";
+          div.style.width = "100px";
+          div.style.height = "100px";
+          div.style.background = "red";
+          div.style.color = "white";
+          div.innerHTML = "Hello";
+          document.body.appendChild(div);
+          alert("Please check all fields");
+          return false;
+          }
     }
