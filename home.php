818387549d50295b24d55cd3cbb258178f15d0b5
Salim Djerbouh <tbitw31@gmail.com>
2016-10-26 00:02:28 +0200

    commit

--- home.php
+++ home.php
@@ -6,7 +6,14 @@ include './inc/header.inc.php';
        <input type="submit" class="btn btn-sm" name="post" value="Post">
      </form>
      <?php
-       $getposts = mysql_query("SELECT * FROM posts WHERE user_posted_to='$username' ORDER BY id DESC LIMIT 10") or die(mysql_errno());
+       $post = @$_POST['postText'];
+       if ($post != "") {
+       $date_added = date("Y-m-d");
+       $added_by = $username;
+       $sqlCommand = "INSERT INTO posts VALUES('', '$post', '$date_added', '$added_by', NULL)";
+       $query = mysql_query($sqlCommand) or die(mysql_error());
+       }
+       $getposts = mysql_query("SELECT * FROM posts WHERE added_by='$username' ORDER BY id DESC LIMIT 10") or die(mysql_errno());
        while ($row = mysql_fetch_assoc($getposts)) {
          $id = $row['id'];
          $body = $row['body'];
@@ -15,11 +22,11 @@ include './inc/header.inc.php';
          $user_posted_to = $row['user_posted_to'];
          echo '<div class="posted_by" ><a href='."$added_by".'>'.$added_by.'</a></div>
          <span style="margin-left: 1em;">'.$date_added.'</span>
-         <br><hr>';
+         <br>
+         '.$body.'
+         <hr>';
      }
       ?>
 <?php
   include './inc/footer.inc.php';
  ?>
-<span></span>
-<a href="#"></a>
