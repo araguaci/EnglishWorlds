a71a4217a28880db5f89cb434029429dc25ddc76
Caddy Dz <tbitw31@gmail.com>
2016-11-21 21:39:27 +0100

    Messages being sent :tada:

--- login.php
+++ login.php
@@ -9,52 +9,38 @@
     header("location: home.php");
   }
 ?>
-<!DOCTYPE html>
-<html>
-  <head lang="en">
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="./css/login.css" media="screen" title="no title">
-    <title>Login</title>
-  </head>
-  <body>
-    <form action="login.php" method="post">
+    <form class="form-horizontal" role="form" action="login.php" method="post">
       <div class="row" id="login">
-      <div class="col-md-4 col-md-offset-4">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-             <span class="glyphicon glyphicon-lock"></span> You must log in to continue.</div>
-          <div class="panel-body">
-          <form class="form-horizontal" role="form">
-            <div class="form-group">
-              <label for="loginMail" class="col-sm-3 control-label">
-               Email</label>
-              <div class="col-sm-9">
-                <input type="text" class="form-control" name="loginMail" id="loginMail" placeholder="Email" required>
+        <div class="col-md-4 col-md-offset-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+            <span class="glyphicon glyphicon-lock"></span> You must log in to continue.</div>
+            <div class="panel-body">
+                <div class="form-group">
+                  <label for="loginMail" class="col-sm-3 control-label">
+                   Email</label>
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control" name="loginMail" id="loginMail" placeholder="Email" required>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="loginPass" class="col-sm-3 control-label">
+                  Password</label>
+                  <div class="col-sm-9">
+                    <input type="password" class="form-control" name="loginPass" id="loginPass" placeholder="Password" required>
+                  </div>
+                </div>
+                <div class="form-group last">
+                  <div class="col-sm-offset-3 col-sm-9">
+                    <input type="submit" name="Login" class="btn btn-success btn-sm" value="Login" >
+                  </div>
+                </div>
+          </div>
+          <div class="panel-footer">Not Registered? <a>Register here</a></div>
+          </div>
               </div>
             </div>
-            <div class="form-group">
-              <label for="loginPass" class="col-sm-3 control-label">
-              Password</label>
-              <div class="col-sm-9">
-                <input type="password" class="form-control" name="loginPass" id="loginPass" placeholder="Password" required>
-              </div>
-            </div>
-            <div class="form-group last">
-              <div class="col-sm-offset-3 col-sm-9">
-                <input type="submit" name="Login" class="btn btn-success btn-sm" value="Login" >
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="panel-footer">
-          Not Registered? <a>Register here</a></div>
-        </div>
-      </div>
-    </div>
-        </div>
-    </form>
-  </body>
-</html>
+      </form>
 <?php
   if (isset($_POST["loginMail"]) && isset($_POST["loginPass"])) {
     $user_login = preg_replace('#[^A-Za-z0-9]#i', '', $_POST["loginMail"]); // filter everything but numbers and letters.
@@ -76,4 +62,5 @@
       echo "Login incorrect, try again";
     }
   }
+  include './inc/footer.inc.php';
  ?>
--- send_msg.php
+++ send_msg.php
@@ -4,7 +4,8 @@
   $user = mysqli_real_escape_string($db, $_GET['u']);
   $buffer=ob_get_contents();
   ob_end_clean();
-  $buffer=str_replace("%TITLE%",$user,$buffer);
+  // TODO: fix this bug
+  $buffer=str_replace("%TITLE%","Message $user",$buffer);
   echo $buffer;
   if (ctype_alnum($user)) {
     // check user exists
@@ -16,14 +17,20 @@
       // Check user isn't sending themselves a private message
       if ($user != $username) {
         if (isset($_POST['submit'])) {
-          $msg_body = mysqli_real_escape_string(@$_POST['msg_body']);
-          $opened = "n";
-          $send_msg = $db->query("INSERT INTO pvt_messages VALUES (NULL, '$username', '$user', '$msg_body', NOW(), '$opened')");
-          echo "Your message has been sent...";
+          $msg_body = strip_tags(@$_POST['msg_body']);
+          $opened = "no";
+          if ($msg_body == "Enter the message you wish to send ...") {
+            echo "Please write a message.";
+          } elseif (strlen($msg_body) < 3) {
+            echo "Your message can not be less than three characters";
+          } else {
+            $send_msg = $db->query("INSERT INTO pvt_messages VALUES (NULL, '$username', '$user', '$msg_body', NOW(), '$opened')");
+            echo "Your message has been sent...";
+          }
         }
         echo '
         <form action="send_msg.php?u=$user" method="POST">
-        <h2>Compose a Message: ($user)</h2>
+        <h2>Compose a Message: ('.$user.')</h2>
         <textarea cols="50" rows="12" name="msg_body">Enter the message you wish to send ...</textarea>
         <input type="submit" name="submit" value="Send Message">
         ';
