f9c8f35c834b24213411f041371f0fc4ca75e106
Caddy Dz <tbitw31@gmail.com>
2016-11-27 21:12:18 +0100

    test CI

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,11 +1,10 @@
 <?php
-
-function loader($class)
-{
-    $file = $class . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-}
-
-spl_autoload_register('loader');
+  function loader($class)
+  {
+      $file = $class . '.php';
+      if (file_exists($file)) {
+          require $file;
+      }
+  }
+  spl_autoload_register('loader');
+?>
