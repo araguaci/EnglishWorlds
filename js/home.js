624bb42a99cb9603c5fa43583b4ac5c90a7ca66c
Caddy Dz <tbitw31@gmail.com>
2016-11-29 22:06:16 +0100

    Added Home JS :sweat_drops:

---
+++ js/home.js
@@ -0,0 +1,10 @@
+// Okay! so get the tabs by a class name and then check for the clicked one by the id and set the class to active by a switch statement
+function SelectTab() {
+  var tab = document.getElementById('PostTab');
+  tab.className = "active";
+  switch (expression) {
+    case expression:
+      break;
+    default:
+    }
+  }
