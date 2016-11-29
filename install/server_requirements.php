983d50257b28d3aa692ab989168a7ef032fd3254
Caddy Dz <tbitw31@gmail.com>
2016-11-29 22:05:19 +0100

    Server requirement :person_frowning:

--- install/server_requirements.php
+++ install/server_requirements.php
@@ -1,45 +1,31 @@
 <?php
-################################################################################
-##              -= YOU MAY NOT REMOVE OR CHANGE THIS NOTICE =-                 #
-## --------------------------------------------------------------------------- #
-##  ApPHP EasyInstaller Free version                                           #
-##  Developed by:  ApPHP <info@apphp.com>                                      #
-##  License:       GNU LGPL v.3                                                #
-##  Site:          http://www.apphp.com/php-easyinstaller/                     #
-##  Copyright:     ApPHP EasyInstaller (c) 2009-2013. All rights reserved.     #
-##                                                                             #
-##  Additional modules (embedded):                                             #
-##  -- jQuery (JavaScript Library)                           http://jquery.com #
-##                                                                             #
-################################################################################
-   
-    session_start();   
+  session_start();
 
-	require_once('include/shared.inc.php');    
-    require_once('include/settings.inc.php');
+	require_once('include/shared.inc.php');
+  require_once('include/settings.inc.php');
 	require_once('include/functions.inc.php');
-	require_once('include/languages.inc.php');	
+	require_once('include/languages.inc.php');
 
 	$task = isset($_POST['task']) ? prepare_input($_POST['task']) : '';
 	$passed_step = isset($_SESSION['passed_step']) ? (int)$_SESSION['passed_step'] : 0;
 	$installation_type = isset($_SESSION['installation_type']) ? $_SESSION['installation_type'] : 'wizard';
 	$program_already_installed = false;
-	
+
 	// handle previous installation
 	// -------------------------------------------------
-    if(file_exists(EI_CONFIG_FILE_PATH)){ 
+    if(file_exists(EI_CONFIG_FILE_PATH)){
 		$program_already_installed = true;
 		///header('location: '.EI_APPLICATION_START_FILE);
         ///exit;
 	}
-	
+
 	// handle previous steps
 	// -------------------------------------------------
 	if($passed_step >= 1){
 		// OK
 	}else{
 		header('location: start.php');
-		exit;				
+		exit;
 	}
 
 	// handle form submission
@@ -50,7 +36,7 @@
 		exit;
 	}
 
-    ob_start();    
+    ob_start();
 	if(function_exists('phpinfo')) @phpinfo(-1);
 	$phpinfo = array('phpinfo' => array());
 	if(preg_match_all('#(?:<h2>(?:<a name=".*?">)?(.*?)(?:</a>)?</h2>)|(?:<tr(?: class=".*?")?><t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>)?)?</tr>)#s', ob_get_clean(), $matches, PREG_SET_ORDER))
@@ -65,10 +51,10 @@
 			$phpinfo[$end_array_keys][] = $match[2];
 		}
 	}
-	
+
 	$is_error = false;
 	$error_mg = array();
-	if(EI_CHECK_PHP_MINIMUM_VERSION && (version_compare(phpversion(), EI_PHP_MINIMUM_VERSION, '<'))){	
+	if(EI_CHECK_PHP_MINIMUM_VERSION && (version_compare(phpversion(), EI_PHP_MINIMUM_VERSION, '<'))){
 		$is_error = true;
 		$alert_min_version_php = lang_key('alert_min_version_php');
 		$alert_min_version_php = str_replace('_PHP_VERSION_', EI_PHP_MINIMUM_VERSION, $alert_min_version_php);
@@ -79,7 +65,7 @@
 		$is_error = true;
 		$error_mg[] = str_replace('_FILE_DIRECTORY_', EI_CONFIG_FILE_DIRECTORY, lang_key('alert_directory_not_writable'));
 	}
-	
+
 	$php_core_index = ((version_compare(phpversion(), '5.3.0', '<'))) ? 'PHP Core' : 'Core';
 	// [0] requred
 	// [1] title
@@ -89,13 +75,13 @@
 	// [5] error message
 	$validations = array(
 		'divider_system_info' => array('title'=>lang_key('getting_system_info'), 'description'=>''),
-		
+
 		'phpversion'   => array(true, lang_key('php_version'), function_exists('phpversion'), phpversion(), lang_key('unknown')),
 		'system'       => array(false, lang_key('system'), isset($phpinfo['phpinfo']['System']), (isset($phpinfo['phpinfo']['System']) ? $phpinfo['phpinfo']['System'] : ''), lang_key('disabled')),
 		'architecture' => array(false, lang_key('system_architecture'), (isset($phpinfo['phpinfo']['Architecture'])), (isset($phpinfo['phpinfo']['Architecture']) ? $phpinfo['phpinfo']['Architecture'] : ''), lang_key('disabled')),
 		'build_date'   => array(false, lang_key('build_date'), isset($phpinfo['phpinfo']['Build Date']), (isset($phpinfo['phpinfo']['Build Date']) ? $phpinfo['phpinfo']['Build Date'] : ''), lang_key('disabled')),
 		'server_api'   => array(false, lang_key('server_api'), isset($phpinfo['phpinfo']['Server API']), (isset($phpinfo['phpinfo']['Server API']) ? $phpinfo['phpinfo']['Server API'] : ''), lang_key('unknown')),
-		
+
 		'divider_php_settings' => array('title'=>lang_key('required_php_settings')),
 		'pdo_support'  => array(false, lang_key('pdo_support'), (isset($phpinfo['PDO']['PDO support']) && $phpinfo['PDO']['PDO support'] == 'enabled'), lang_key('enabled'), lang_key('disabled'), lang_key('error_pdo_support')),
 		'database_extension' => array(false, lang_key('database_extension').' (pdo_'.EI_DATABASE_TYPE.')', extension_loaded('pdo_'.EI_DATABASE_TYPE), lang_key('enabled'), lang_key('disabled')),
@@ -110,7 +96,7 @@
 	if(EI_CHECK_MBSTRING_SUPPORT){
 		$validations['mbstring_support'] = array(false, lang_key('mbstring_support'), function_exists('mb_detect_encoding'), lang_key('enabled'), lang_key('disabled'));
 	}
-	
+
 	if(EI_CHECK_MAGIC_QUOTES){
 		$validations['divider_magic_quotes'] = array('title'=>'', 'description'=>'');
 		$validations['magic_quotes_gpc'] = array(false, lang_key('magic_quotes_gpc'), ini_get('magic_quotes_gpc'), lang_key('on'), lang_key('off'));
@@ -125,7 +111,7 @@
 		$validations['sendmail_from'] = array(false, lang_key('sendmail_from'), ini_get('sendmail_from'), ini_get('sendmail_from'), lang_key('unknown'));
 		$validations['sendmail_path'] = array(false, lang_key('sendmail_path'), ini_get('sendmail_path'), ini_get('sendmail_path'), lang_key('unknown'));
 	}
-	
+
 	if(EI_CHECK_EXTENSIONS){
 		$validations['divider_extensions'] = array('title'=>lang_key('extensions'), 'description'=>'');
 		$loaded_extensions = get_loaded_extensions();
@@ -133,17 +119,17 @@
 		$validations['php_xmlrpc'] = array(false, 'PHP XmlRrpc', in_array('xmlrpc', $loaded_extensions), lang_key('installed'), lang_key('not_installed'));
 		$validations['php_gd2'] = array(false, 'PHP Gd2', in_array('gd2', $loaded_extensions), lang_key('installed'), lang_key('not_installed'));
 	}
-	
+
 	if(EI_CHECK_MODES){
 		$validations['divider_modes'] = array('title'=>lang_key('modes'), 'description'=>'');
 		$loaded_extensions = get_loaded_extensions();
 		$validations['mod_rewrite'] = array(false, lang_key('mode').' Rewrite', in_array('rewrite', $loaded_extensions), lang_key('installed'), lang_key('not_installed'));
 		$validations['mod_ldap'] = array(false, lang_key('mode').' LDAP', in_array('ldap', $loaded_extensions), lang_key('installed'), lang_key('not_installed'));
 	}
-	
+
 	if(EI_CHECK_DIRECTORIES_AND_FILES){
 		$validations['divider_dirs_and_files'] = array('title'=>lang_key('directories_and_files'), 'description'=>'');
-		$validations['config_file_dir'] = array(true, EI_CONFIG_FILE_DIRECTORY, is_writable(EI_CONFIG_FILE_DIRECTORY), lang_key('writable'), lang_key('no_writable'));		
+		$validations['config_file_dir'] = array(true, EI_CONFIG_FILE_DIRECTORY, is_writable(EI_CONFIG_FILE_DIRECTORY), lang_key('writable'), lang_key('no_writable'));
 		///$validations['cache_file_dir'] = array(false, '../cache/', is_writable('../cache/'), lang_key('writable'), lang_key('no_writable'));
 	}
 
@@ -156,7 +142,7 @@
     <meta name="author" content="ApPHP Company - Advanced Power of PHP">
     <meta name="generator" content="ApPHP EasyInstaller">
 	<title><?php echo lang_key('installation_guide'); ?> | <?php echo lang_key('server_requirements'); ?></title>
-	
+
 	<link href="images/apphp.ico" rel="shortcut icon" />
 	<link rel="stylesheet" type="text/css" href="templates/<?php echo EI_TEMPLATE; ?>/css/styles.css" />
 	<?php
@@ -176,7 +162,7 @@
 	<div id="content">
 		<?php if($installation_type == 'wizard'){ ?>
 			<?php
-				draw_side_navigation(2);		
+				draw_side_navigation(2);
 			?>
 			<div class="central-part">
 			<h2><?php echo lang_key('step_2_of'); ?> - <?php echo lang_key('server_requirements'); ?></h2>
@@ -188,13 +174,13 @@
 				<?php
 					$content = '';
 					foreach($validations as $key => $val){
-						$content .= '<tr>';							
+						$content .= '<tr>';
 						if(preg_match('/divider\_/i', $key)){
 							if($val['title'] != ''){
 								$content .= '<td colspan="2">';
 								$content .= '<h3>'.$val['title'].'</h3>';
 								if(!empty($val['description'])) $content .= '<p>'.$val['description'].'</p>';
-								$content .= '</td>';	
+								$content .= '</td>';
 							}else{
 								$content .= '<td colspan="2" nowrap height="9px"></td>';
 							}
@@ -205,13 +191,13 @@
 								$error_mg[$key] = isset($val[5]) ? $val[5] : str_ireplace('_SETTINGS_NAME_', '<b>'.$key.'</b>', lang_key('error_server_requirements'));
 								$content .= '<td><span class="failed">'.lang_key('failed').'!</span></td>';
 							}else{
-								$content .= '<td><span class="passed">'.lang_key('passed').'</span></td>';	
+								$content .= '<td><span class="passed">'.lang_key('passed').'</span></td>';
 							}
 						}
 						$content .= '</tr>'."\n";
-					}				
+					}
 				?>
-				
+
 				<?php
 					if($is_error){
 						echo '<div class="alert alert-error">';
@@ -221,7 +207,7 @@
 						echo '</div>';
 					}
 					if(!$is_error && $program_already_installed){
-						echo '<div class="alert alert-warning">'.lang_key('alert_unable_to_install').'</div>';									
+						echo '<div class="alert alert-warning">'.lang_key('alert_unable_to_install').'</div>';
 					}
 				?>
 				<table width="99%" cellspacing="2" cellpadding="0" border="0">
@@ -229,7 +215,7 @@
 				<?php echo $content; ?>
 				</tbody>
 				</table>
-				
+
 				<div class="buttons-wrapper">
 					<a href="start.php" class="form_button" /><?php echo lang_key('back'); ?></a>
 					<?php if(!$is_error){ ?>
@@ -240,7 +226,7 @@
 			</form>
 			</div>
 		<?php
-			}else{			
+			}else{
 				if(EI_ALLOW_MANUAL_INSTALLATION){
 					echo '<div id="divManually">';
 					echo '<div class="content">';
@@ -249,11 +235,11 @@
 					echo '<div class="footer"><a href="start.php" class="form_button" title="'.lang_key('cancel_installation').'" />'.lang_key('back').'</a></div>';
 					echo '</div>';
 				}
-			} 	
-		?>			
+			}
+		?>
 		<div class="clear"></div>
 	</div>
-	
+
 	<?php include_once('include/footer.inc.php'); ?>
 
 </div>
