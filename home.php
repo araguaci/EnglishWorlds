cb70b3eb336b2e20d1cdc29f91500ca57392b300
Caddy Dz <tbitw31@gmail.com>
2016-12-02 19:33:54 +0100

    Style Home Page  :octocat:

--- home.php
+++ home.php
@@ -1,56 +1,72 @@
 <?php
-ob_start();
-require_once './inc/header.inc.php';
-if (!$username) {
-  echo '<div class="alert alert-danger" role="alert">You must be logged in to view this page nigga!!</div>';
-  die();
-}
-$buffer = ob_get_contents();
-ob_end_clean();
-$buffer=str_replace("%TITLE%","Home Feed",$buffer);
-echo $buffer;
-require('./inc/westsworld.datetime.class.php');
-require('./inc/timeago.inc.php');
- ?>
-    <div class="container">
-      <form id="postingForm" action="home.php" method="post">
-        <ul class="nav nav-tabs">
-          <li id="PostTab" role="presentation" onclick="javascript:SelectTab()"><a href="#">Write Post</a></li>
-          <li id="PhotoTab" role="presentation"><a href="#" onclick="javascript:SelectTab()">Add Photo / Video</a></li>
-          <li id="PollTab" role="presentation"><a href="#" onclick="javascript:SelectTab()">Create poll</a></li>
-        </ul>
-        <textarea name="postText" class="input-medium" placeholder="Share your thoughts..."></textarea>
-        <button class="btn" type="submit">Post</button>
-      </form>
+  ob_start();
+  require_once './inc/header.inc.php';
+  if (!$username) {
+    LoginAlert();
+  }
+  $buffer = ob_get_contents();
+  ob_end_clean();
+  $buffer=str_replace("%TITLE%","Home Feed",$buffer);
+  echo $buffer;
+  require('./inc/westsworld.datetime.class.php');
+  require('./inc/timeago.inc.php');
+  $post = @$_POST['PostText'];
+  if ($post != "") {
+  $date_added = date("Y-m-d");
+  $time = date("hh:ii:ss", time());
+  $added_by = $username;
+  $sqlCommand = "INSERT INTO posts VALUES('', '$post', '$date_added', NOW(), '$added_by', NULL)";
+  $query = $db->query($sqlCommand) or die(mysql_error());
+  }
+  $getposts = $db->query("SELECT * FROM posts WHERE added_by='$username' ORDER BY id DESC LIMIT 10") or die(mysql_errno());
+  $getname = $db->query("SELECT first_name, last_name FROM users WHERE username='$username'");
+  $readname = $getname->fetch_assoc();
+  $first_name = $readname['first_name'];
+  $last_name = $readname['last_name'];
+  $current_time = date('Y/m/d H:i:s', time());
+  ?>
+    <center>
+      <div class="container">
+        <div class="NewsFeed">
+          <h2>News Feed</h2>
+        </div>
+        <div id="PostTabs" style="width: 600px; height: 50px;">
+          <ul class="nav nav-tabs">
+            <li class="nav active"><a href="#Post" data-toggle="tab">Post</a></li>
+            <li class="nav"><a href="#Photo" data-toggle="tab">Share Photo</a></li>
+            <li class="nav"><a href="#Video" data-toggle="tab">Share Video</a></li>
+            <li class="nav"><a href="#Poll" data-toggle="tab">Create Poll</a></li>
+            <li class="nav"><a href="#Upload" data-toggle="tab">Upload File</a></li>
+          </ul>
+          <div class="tab-content">
+              <div class="tab-pane fade in active" id="Post">
+                <form action="home.php" method="post">
+                  <input type="text" name="PostText" class="input-medium" placeholder="Share your thoughts">
+                  <button class="btn btn-sm" type="submit">Post</button>
+                </form>
+              </div>
+              <div class="tab-pane fade" id="Photo">There should be a photo posting form here</div>
+              <div class="tab-pane fade" id="Video">There should be a video posting form here</div>
+              <div class="tab-pane fade" id="Poll">There should be a poll posting form here</div>
+              <div class="tab-pane fade" id="Upload">There should be a upload posting form here</div>
+          </div>
+        </div><br><br>
+      <?php
+      while ($row = $getposts->fetch_assoc()) {
+        $id = $row['id'];
+        $body = $row['body'];
+        $date_added = $row['date_added'];
+        $time_added = $row['time'];
+        $added_by = $row['added_by'];
+        $user_posted_to = $row['user_posted_to'];
+        $timeAgo = new TimeAgo();
+        echo '<div class="PostedBy" ><a href='."$added_by".'>'.capitalize($first_name).' '.capitalize($last_name).'</a></div>
+        <span style="margin-left: 1em;">'.$timeAgo->inWords($time_added, $current_time).'</span>
+        <br>
+        '.$body.'
+        <hr>';
+      }
+     ?>
     </div>
-     <?php
-       $post = @$_POST['postText'];
-       if ($post != "") {
-       $date_added = date("Y-m-d");
-       $time = date("hh:ii:ss", time());
-       $added_by = $username;
-       $sqlCommand = "INSERT INTO posts VALUES('', '$post', '$date_added', NOW(), '$added_by', NULL)";
-       $query = $db->query($sqlCommand) or die(mysql_error());
-       }
-       $getposts = $db->query("SELECT * FROM posts WHERE added_by='$username' ORDER BY id DESC LIMIT 10") or die(mysql_errno());
-       $getname = $db->query("SELECT first_name, last_name FROM users WHERE username='$username'");
-       $readname = $getname->fetch_assoc();
-       $first_name = $readname['first_name'];
-       $last_name = $readname['last_name'];
-       $current_time = date('Y/m/d H:i:s', time());
-       while ($row = $getposts->fetch_assoc()) {
-         $id = $row['id'];
-         $body = $row['body'];
-         $date_added = $row['date_added'];
-         $time_added = $row['time'];
-         $added_by = $row['added_by'];
-         $user_posted_to = $row['user_posted_to'];
-         $timeAgo = new TimeAgo();
-         echo '<div class="posted_by" ><a href='."$added_by".'>'.capitalize($first_name).' '.capitalize($last_name).'</a></div>
-         <span style="margin-left: 1em;">'.$timeAgo->inWords($time_added, $current_time).'</span>
-         <br>
-         '.$body.'
-         <hr>';
-     }
-  include './inc/footer.inc.php';
- ?>
+  </center>
+<?php include './inc/footer.inc.php'; ?>
