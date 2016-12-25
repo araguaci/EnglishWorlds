a5fdfea11d43ce0767aa375045d67342618dde25
Salim Djerbouh <caddydz4@gmail.com>
2016-12-25 23:50:35 +0100

    test

--- send_post.php
+++ send_post.php
@@ -11,7 +11,7 @@ if (isset($_SESSION["user_login"])) {
    $date_added = date("Y-m-d");
    $added_by = $username;
    $user_posted_to = "test123";
-   $sqlCommand = "INSERT INTO posts VALUES('', '$post', '$date_added', '$added_by', '$user_posted_to')";
+   $sqlCommand = "INSERT INTO posts VALUES(NULL, '$post', '$date_added', '$added_by', '$user_posted_to')";
    $query = $db->query($sqlCommand) or die(mysql_error());
  } else {
    echo "You must enter something in the post field before you can send it ....";
