3a9f6002444a4b3b666766c2c0c3d705dd665841
Salim Djerbouh <caddydz4@gmail.com>
2016-12-25 23:55:25 +0100

    test

--- send_post.php
+++ send_post.php
@@ -17,3 +17,4 @@ if (isset($_SESSION["user_login"])) {
    echo "You must enter something in the post field before you can send it ....";
  }
  ?>
+this fucking not working properly at all god damn it -_- 
