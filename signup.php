6bee3dfae4fdcde1f2c8570763797b1c192fa88b
Salim Djerbouh <caddydz4@gmail.com>
2016-12-25 22:06:45 +0100

    test

--- signup.php
+++ signup.php
@@ -10,6 +10,7 @@
     <div class="panel panel-default">
       <div class="panel-heading">
         Registration
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </div>
       <div class="panel-body">
          <form class="" action="index.html" method="post">
