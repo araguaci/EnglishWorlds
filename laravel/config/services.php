19ad7f93c49f0d561f316f8a02ae2bbb4eccd81a
Salim Djerbouh <tbitw31@gmail.com>
2017-01-19 21:49:38 +0100

    Renamed app :bowtie:

--- laravel/config/services.php
+++ laravel/config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => App\User::class,
+        'model' => English\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
