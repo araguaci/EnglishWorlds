cad58742fed7394a5ecc3b065855e7a2248f31b6
Salim Djerbouh <tbitw31@gmail.com>
2017-02-23 22:14:46 +0100

    MVC Insert function Added basic abstracted function to insert items to DB

--- MVC/Registry/mysqldb.class.php
+++ MVC/Registry/mysqldb.class.php
@@ -121,5 +121,33 @@
 
       return true;
     }
+    /**
+    * Insert records into the database
+    * @param String the database table
+    * @param array data to insert field => value
+    * @return bool
+    */
+    public function insertRecords($table, $data)
+    {
+      // setup some variables for fields and values
+      $fields = "";
+      $values = "";
+
+      // populate them
+      foreach ($data as $f => $v) {
+        $fields .= "`$f`,";
+        $values .= (is_numeric($v) && (intval($v) == $v)) ? $v."," : "'$v',";
+      }
+
+      // remove our trailing ,
+      $fields = substr($fields, 0, -1);
+      // remove our trailing ,
+      $values = substr($values, 0, -1);
+
+      $insert = "INSERT INTO $table ({$fields}) VALUES({values})";
+      // echo $insert;
+      $this->executeQuery($Insert);
+      return true;
+    }
 }
 ?>
