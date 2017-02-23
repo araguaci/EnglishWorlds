323c7e3a13fa7f19d8752f6d1fe9d88dfdf97372
Salim Djerbouh <tbitw31@gmail.com>
2017-02-23 23:58:39 +0100

    Clear cache

--- MVC/registry/template.class.php
+++ MVC/registry/template.class.php
@@ -47,5 +47,62 @@
          }
          $this->page->addTemplateBit($tag, $bit);
        }
+      /**
+       * Take the template bits from the view and insert them into our page content
+       * Updates the pages content
+       * @return void
+       */
+       private function repkaceBits()
+       {
+         $bits = $this->page->getBits();
+         // loop through template bits
+         foreach ($bits as $tag => $template) {
+           $templateContent = file_get_contents($template);
+           $newContent = str_replace('{' . $tag . '}', $templateContent, $this->page->getContent());
+           $this->page->setContent($newContent);
+         }
+       }
+      /**
+       * Replace tags in our page with content
+       * @return void
+       */
+      private function replaceTags($pp = false)
+      {
+        // get the tags in the page
+        if ($pp == false) {
+          $tags = $this->page->getTags();
+        } else {
+          $tags = $this->page->getPPTags();
+        }
+        // go through them all
+        foreach ($tags as $tag => $data) {
+          //if the tag is an array, then we need to do more than a simple find and replace!
+          if (is_array($data)) {
+            if($data[0] == 'SQL'){
+              // it is a cached query...replace tags from the database
+              $this->replaceDBTags($tag, $data[1]);
+            } elseif ($data[0] == 'DATA') {
+              // it is some cached data...replace tags from cached data
+              $this->replaceDataTags($tag, $data[1]);
+            }
+          }
+          else {
+            // replace the content
+            $newContent = str_replace('{' . $tag . '}', $data, $this->page->getContent());
+            // update the pages content
+            $this->page->setContent($newContent);
+          }
+        }
+      }
+     /**
+      * Replace content on the page with data from the database
+      * @param String $tag the tag defining the area of content
+      * @param int $cachedId the queries ID in the query cache
+      * @return void
+      */
+      private function replaceDBTags($tag, $cachedId)
+      {
+        
+      }
     }
  ?>
