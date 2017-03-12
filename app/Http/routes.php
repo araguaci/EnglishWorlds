74ba125ce48f1a237a52da875e4a027c22bf0bd9
Salim Djerbouh <tbitw31@gmail.com>
2017-03-12 19:11:20 +0100

    Flash messages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,5 +3,8 @@
 Route::get('/', [
   'uses' => '\English\Http\Controllers\HomeController@index',
   'as' => 'home',
-  
 ]);
+
+Route::get('/alerts', function() {
+  return redirect()->route('home')->with('info', 'You have signed up!');
+});
