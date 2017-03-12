249e3917c33c693fce567ca7eac66483506338fa
Salim Djerbouh <tbitw31@gmail.com>
2017-03-12 19:04:19 +0100

    Home controller and base template

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+namespace English\Http\Controllers;
+
+/**
+ * @author Salim Djerbouh
+ * @version 0.1
+ */
+class HomeController extends Controller
+{
+  public function index()
+  {
+    return view('home');
+  }
+}
+
+ ?>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('templates.default')
+
+@section('content')
+  <h1>Welcome to English Dz</h1>
+  <p>The first Algerian social network for English Speakers</p>
+@stop
