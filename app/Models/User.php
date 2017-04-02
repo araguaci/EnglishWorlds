3afe7e867039afb2835c895ff02ded1a1a742157
Salim Djerbouh <tbitw31@gmail.com>
2017-04-02 22:15:59 +0100

    Friend model methods Signed-off-by: Salim Djerbouh <tbitw31@gmail.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,4 +67,38 @@ class User extends Model implements AuthenticatableContract{
     {
       return $this->friendsOfMine()->wherePivot('accepted', false)->get();
     }
+
+    public function friendRequestPending()
+    {
+      return $this->friendOf()->wherePivot('accepted', false())->get();
+    }
+
+    public function hadFriendRequestPending(User $user)
+    {
+      return (bool) $this->friendRequestsPending()->where('id', $user->id)->count();
+    }
+
+    public function hasFriendRequestReceived(User $user)
+    {
+      return (bool) $this->friendRequests()->where('id', $user->id)->count();
+    }
+
+    public function addFriend(User $user)
+    {
+      $this->friendOf()->attach($user->id);
+    }
+
+    public function acceptFriendRequest(User $user)
+    {
+      $this->friendRequests()->where('id', $user->id)->first()->pivot->update([
+        'accepted' => true,
+      ]);
+    }
+
+    public function isFriendsWith(User $user)
+    {
+      return (bool) $this->friends()->where('id', $user->id)->count();
+    }
+
+
 }
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -5,9 +5,19 @@
     <div class="col-lg-5">
       @include('user.partials.userblock')
       <hr>
+
     </div>
     <div class="col-lg-4 col-lg-offset-3">
-      <h4>{{ $user->getFirstNameOrUsername() }}'s friends</h4>
+      @if (Auth::user()->hadFriendRequestPending($user))
+        <p>Waiting for {{ $user->getNameOrUsername() }} to accept your request.</p>
+      @elseif (Auth::user()->hasFriendRequestReceived($user))
+        <a href="#" class="btn btn-primary">Accept friend request</a>
+      @elseif (Auth::user()->isFriendsWith($user))
+        <p>You and {{ $user->getNameOrUsername() }} are friends.</p>
+      @else
+        <a href="#" class="btn btn-primary">Add as friend</a>
+      @endif
+      <h4>{{ $user->getFirstNameOrUsername() }}'s friends.</h4>
 
       @if (!$user->friends()->count())
         <p>{{ $user->getFirstNameOrUsername() }} has no friends.</p>
