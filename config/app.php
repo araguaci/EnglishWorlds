d59d3d8d10f5e8179ca028066a0897159b2ac387
Salim Djerbouh <tbitw31@gmail.com>
2017-04-10 18:29:54 +0100

    Application namespace set :bookmark: Signed-off-by: Salim Djerbouh <tbitw31@gmail.com>

--- config/app.php
+++ config/app.php
@@ -171,11 +171,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        English\Providers\AppServiceProvider::class,
+        English\Providers\AuthServiceProvider::class,
+        // English\Providers\BroadcastServiceProvider::class,
+        English\Providers\EventServiceProvider::class,
+        English\Providers\RouteServiceProvider::class,
 
     ],
 
