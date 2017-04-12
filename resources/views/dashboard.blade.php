b98d099f2ffaea9abf573beb3cd5a7d98eed5a53
Salim Djerbouh <tbitw31@gmail.com>
2017-04-12 14:38:56 +0100

    Dashboard view :lipstick: Signed-off-by: Salim Djerbouh <tbitw31@gmail.com>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,44 @@
-<h1>The dashboard</h1>
+@extends('layouts.master')
+
+@section('content')
+  <section class="row new-post">
+    <div class="col-md-6 col-md-offset-3">
+      <header><h3>What do you have to say?</h3></header>
+      <form class="" action="" method="post">
+        <div class="form-group">
+          <textarea name="new-post" class="form-control" id="new-post" rows="5" cols="80" placeholder=""></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Create Post</button>
+      </form>
+    </div>
+  </section>
+  <section class="row posts">
+    <div class="col-md-6 col-md-offset-3">
+      <header><h3>What other people say...</h3></header>
+      <article class="post">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <div class="info">
+          Posted by Salim on 12 April 2017
+        </div>
+        <div class="interaction">
+          <a href="#">Like</a>
+          <a href="#">Dislike</a>
+          <a href="#">Edit</a>
+          <a href="#">Delete</a>
+        </div>
+      </article>
+      <article class="post">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <div class="info">
+          Posted by Salim on 12 April 2017
+        </div>
+        <div class="interaction">
+          <a href="#">Like</a>
+          <a href="#">Dislike</a>
+          <a href="#">Edit</a>
+          <a href="#">Delete</a>
+        </div>
+      </article>
+    </div>
+  </section>
+@endsection
