d07546538dacac7c9af55564546145c322b55af4
Salim Djerbouh <tbitw31@gmail.com>
2017-04-17 21:09:30 +0100

    Retreive all posts :clipboard: Signed-off-by: Salim Djerbouh <tbitw31@gmail.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,13 +23,10 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-      if (Auth::check()) {
-        $statuses = Status::notReply()->where(function($query){
-          // FIXME: Method lists from the function friends() in User:58 does not exist
-          return $query->where('user_id', Auth::user()->id)->orWhereIn('user_id', Auth::user()->friends()->all('id')); })->orderBy('created_at', 'desc')->paginate(10);
+    public function index() {
 
+      if (Auth::check()) {
+        $statuses = Status::notReply()->orderBy('created_at', 'desc')->paginate(10);
         return view('timeline.index')->with('statuses', $statuses);
       }
       return view('home');
