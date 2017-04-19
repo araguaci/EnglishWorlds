c1bbf31b47383928c6b2499e679a92ddbcdfb3e5
Salim Djerbouh <tbitw31@gmail.com>
2017-04-19 09:17:01 +0100

    Fix some styling :lipstick: Signed-off-by: Salim Djerbouh <tbitw31@gmail.com>

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -24,7 +24,7 @@
             </ul>
             <form class="navbar-form navbar-left" role="search" action="{{ route('search.results') }}">
               <div class="form-group">
-                <input type="text" name="query" class="form-control" placeholder="Find people">
+                <input type="text" name="query" class="form-control" placeholder="Find people" required>
               </div>
               <button type="submit" class="btn btn-default">Search</button>
             </form>
