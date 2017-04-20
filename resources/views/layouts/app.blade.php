13952efb8a3480097e0ab3e81d1703348ea647f0
5nai3r <dzmakr@gmail.com>
2017-04-20 13:34:59 +0100

    Responsive Nav
    add costum responsive nav bar ✌️

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/timeline.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/welcome.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
 
     <!-- Scripts -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,16 @@
 @section('content')
     
         <div class="row usr-wlcm">
-            <div class="col-8 offset-2">
+            <div class="col-md-8 offset-md-2">
                 <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             </div>
         </div>
         <div class="row usr-login">
-            <div class="col-3 offset-2 usr-login-sec">
+            <div class="col-lg-3 col-sm-8 offset-lg-2 offset-sm-2 usr-login-sec">
                 <p>Existing user?</p>
                 <a class="usr-btn" href="{{ url('/login') }}">Login</a>
             </div>
-            <div class="col-3 offset-2 usr-register-sec">
+            <div class="col-lg-3 col-sm-8 offset-lg-2 offset-sm-2 usr-register-sec">
                 <p>New user?</p>
                 <a class="usr-btn" href="{{ url('/register') }}">Register</a>
             </div>
