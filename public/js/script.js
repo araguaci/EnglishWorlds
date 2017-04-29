6ed40703b1eb5bdfb75a10575502edbc0278616c
Salim Djerbouh <tbitw31@gmail.com>
2017-04-29 14:00:17 +0100

    Fix JS Script bug :bug: Signed-off-by: Salim Djerbouh <tbitw31@gmail.com>

--- public/js/script.js
+++ public/js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 			var stsBody = $('#status').val();
 			var dataString = "status="+stsBody;
 			$.ajax({
-				type: "POST",sty
+				type: "POST",
 				url: 'status',
 				data: dataString,
 				success: function(data){
