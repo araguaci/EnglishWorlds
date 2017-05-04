84582cc0f73b5b4a5917411014b568b8c875fe54
Salim Djerbouh <tbitw31@gmail.com>
2017-05-04 02:44:43 +0100

    Friends foreign key :key: constraint Signed-off-by: Salim Djerbouh <tbitw31@gmail.com>

--- database/migrations/2017_04_17_103920_create_friends_table.php
+++ database/migrations/2017_04_17_103920_create_friends_table.php
@@ -15,7 +15,8 @@ class CreateFriendsTable extends Migration
     {
         Schema::create('friends', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
+            $table->integer('user_id')->nullable()->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->integer('friend_id');
             $table->boolean('accepted')->default(0);
             $table->timestamps();
