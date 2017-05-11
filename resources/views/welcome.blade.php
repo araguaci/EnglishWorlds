8b1f90760508e1f7bd9695bdbebe19dbc42e1fb9
Salim Djerbouh <tbitw31@gmail.com>
2017-05-11 01:07:04 +0100

    Fix Welcome view style :lipstick: Signed-off-by: Salim Djerbouh <tbitw31@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
 	<div class="ui grid">
-        <div class="row">
-            <div class="column padding-reset">
-                <div class="ui huge message page grid">
+				<div class="row">
+						<div class="column padding-reset">
+								<div class="ui huge message page grid">
 									<div id="header" class="ui huge header">
 										<img src="{{ asset('img/brand.png') }}" id="logo" alt="English DZ Logo">
 										<span id="websiteTitle">{{ config('app.name') }}</span>
 										<p id="websiteSlogan">Learn English in Algeria, the fun way</p>
 									</div>
-                </div>
-            </div>
-        </div>
-    </div>
+								</div>
+						</div>
+				</div>
+		</div>
 		<div class="ui raised very padded text container segment">
 			<h1>Algerian English speakers social network</h1>
 		</div>
@@ -25,45 +25,47 @@
 			<div class="ui two column centered grid">
 				<div class="ui special link cards">
 						<div class="card">
-					    <div class="blurring dimmable image">
-					      <div class="ui dimmer">
-					        <div class="content">
-					          <div class="center">
+							<div class="blurring dimmable image">
+								<div class="ui dimmer">
+									<div class="content">
+										<div class="center">
 											<a class="ui inverted button" href="{{ url('/login') }}">Login</a>
-					          </div>
-					        </div>
-					      </div>
-					      <img src="{{ asset('img/user.svg') }}">
-					    </div>
-					    <div class="content">
-					      <a class="header">New user?</a>
-					  	</div>
+										</div>
+									</div>
+								</div>
+								<img src="{{ asset('img/user.svg') }}">
+							</div>
+							<div class="content">
+								<a class="header">New user?</a>
+							</div>
 						</div>
 						<div class="card">
-					    <div class="blurring dimmable image">
-					      <div class="ui dimmer">
-					        <div class="content">
-					          <div class="center">
+							<div class="blurring dimmable image">
+								<div class="ui dimmer">
+									<div class="content">
+										<div class="center">
 											<a class="ui inverted button" href="{{ url('/register') }}">Register</a>
-					          </div>
-					        </div>
-					      </div>
-					      <img src="{{ asset('img/user.svg') }}">
-					    </div>
-					    <div class="content">
-					      <a class="header">Existing user?</a>
-					  	</div>
+										</div>
+									</div>
+								</div>
+								<img src="{{ asset('img/user.svg') }}">
+							</div>
+							<div class="content">
+								<a class="header">Existing user?</a>
+							</div>
 						</div>
 					</div>
 				</div>
 		</div>
 	@endif
+	@include('layouts.footer')
 @endsection
 
 @section('styles')
-  <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/footer.css') }}" rel="stylesheet">
 @endsection
 
 @section('scripts')
-  <script src="{{ asset('js/welcome.js') }}" charset="utf-8"></script>
+	<script src="{{ asset('js/welcome.js') }}" charset="utf-8"></script>
 @endsection
