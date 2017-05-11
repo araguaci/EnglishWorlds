bed15ebeb685cffed946d7142c6d236da921bcb6
Salim Djerbouh <tbitw31@gmail.com>
2017-05-11 01:05:43 +0100

    Fix Friends list view :lipstick: Signed-off-by: Salim Djerbouh <tbitw31@gmail.com>

--- resources/views/friends/index.blade.php
+++ resources/views/friends/index.blade.php
@@ -1,28 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="row">
-    <div class="col-lg-6">
-      <h3>Your friends</h3>
-
-      @if (!$friends->count())
-        <p>You have no friends.</p>
-      @else
+  <div class="ui two column centered grid">
+    <div class="four column centered row">
+      <div class="column">
+        <h3>Your friends</h3>
+        @if (!$friends->count())
+            <p>You have no friends.</p>
+        @else
           @foreach ($friends as $user)
-              @include('user.partials.userblock')
+            @include('user.partials.userblock')
           @endforeach
-      @endif
-    </div>
-    <div class="col-lg-6">
-      <h4>Friends requests</h4>
-
-      @if (!$requests->count())
-        <p>You have no friend requests.</p>
-      @else
-        @foreach ($requests as $user)
-          @include('user.partials.userblock')
-        @endforeach
-      @endif
+        @endif
+      </div>
+      <div class="column">
+        <h4>Friends requests</h4>
+        @if (!$requests->count())
+          <p>You have no friend requests.</p>
+        @else
+          @foreach ($requests as $user)
+            @include('user.partials.userblock')
+          @endforeach
+        @endif
+      </div>
     </div>
   </div>
 @stop
