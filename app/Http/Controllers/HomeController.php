e15d052c5c638e02569303b8854c6af3f0485095
Caddy Dz <tbitw31@gmail.com>
2017-06-09 14:58:41 +0100

    Refactor posts fetching method
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
     public function index()
     {
         if (Auth::check()) {
-            $statuses = Status::notReply()->orderBy('created_at', 'desc')->paginate(10);
+            $statuses = Status::notReply()->latest()->paginate(10);
 
             return view('timeline.index')->with('statuses', $statuses);
         }
