4652b3a24be9e634bf8b8a3a4471cd69b6095d9e
Caddy Dz <tbitw31@gmail.com>
2017-06-11 22:23:00 +0100

    Test heroku
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- config/database.php
+++ config/database.php
@@ -62,7 +62,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '192.168.8.1'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
