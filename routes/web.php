766ea5be90976fac2f276ebf31ad667d459eb0eb
Mohamed Amine <MohamedAmine1C@gmail.com>
2017-06-16 23:41:29 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('userimage/{filename}', [
 ]);
 
 
-Route::middleware(['auth'])->group(function() {
+Route::middleware(['auth'])->group(function () {
     Route::get('profile/edit', [
         'uses' => 'ProfileController@getEdit',
         'as' => 'profile.edit',
@@ -59,7 +59,7 @@ Route::middleware(['auth'])->group(function() {
  */
 
 // Post statuses
-Route::middleware(['auth'])->group(function() {
+Route::middleware(['auth'])->group(function () {
     Route::post('status', [
         'uses' => 'StatusController@postStatus',
         'as' => 'status.post'
