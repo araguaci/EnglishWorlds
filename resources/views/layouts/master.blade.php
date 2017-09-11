2baf1131b94b983e590d041f1e8a4109ea80db20
Caddy Dz <tbitw31@gmail.com>
2017-09-11 18:57:07 +0100

    Update favicon path
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name') }}</title>
-        <link rel="icon" type="image/ico" href="favicon.ico">
+        <link rel="icon" type="image/ico" href="/favicon.ico">
         <!-- Local -->
         <link rel="stylesheet" type="text/css" href="/css/raw.min.css">
         <link rel="stylesheet" type="text/css" href="/css/semantic.min.css">
