f02f47eac5d9ba2b47706082420962933a5a6e7c
Caddy Dz <tbitw31@gmail.com>
2018-02-16 19:03:42 +0100

    Postgresql connection on deploy
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- config/database.php
+++ config/database.php
@@ -1,5 +1,11 @@
 <?php
 
+$url = parse_url(env("DATABASE_URL"));
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -62,11 +68,11 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', '127.0.0.1'),
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'port'     => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
