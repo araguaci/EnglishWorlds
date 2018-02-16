28ebf82c7e92f2561c82f7878f763c1d87e1cae7
Caddy Dz <tbitw31@gmail.com>
2018-02-16 18:26:52 +0100

    Redirect http to https
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,7 +16,8 @@ class HttpsProtocol
     public function handle($request, Closure $next)
     {
         if (!$request->secure() && env('APP_ENV') === 'production') {
-            return redirect()->secure($request->getRequestUri());
+            $request->setTrustedProxies( [ $request->getClientIp() ] );
+            return redirect()->secure($request->getRequestUri(), 301);
         }
         return $next($request);
     }
