9e57edc1632c4b51bd7d23fc0da0d18015af4801
Caddy Dz <tbitw31@gmail.com>
2018-03-24 10:30:32 +0100

    Document test case
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

---
+++ tests/TestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+    use DatabaseMigrations;
+
+    /**
+     * Set the currently logged in user for the application.
+     * @param Illuminate\Contracts\Auth\Authenticatable
+     * @return English\User authenticated
+     */
+
+    protected function login($user)
+    {
+        $this->be($user);
+    }
+}
