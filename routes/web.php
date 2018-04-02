bbf69ce89d8a157f0a098f9baed5d8802be90378
Caddy Dz <tbitw31@gmail.com>
2018-04-02 09:48:05 +0100

    Update routes
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
-Route::get('/', 'StatusesController@index');
-Route::post('/', 'StatusesController@store');
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/', 'StatusesController@index');
+Route::post('/', 'StatusesController@store');
 Route::get('/{status}', 'StatusesController@show');
 
 Route::post('/{status}/comment', 'CommentsController@store')->name('post_comment');
