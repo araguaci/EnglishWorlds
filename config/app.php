41c52bca7bce243fe43866a1bc10d5b7c55ec056
Salim Djerbouh <github@caddydz.me>
2018-04-07 20:52:20 +0100

    Work In Progress
    This is a trash commit to save changes
    
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- config/app.php
+++ config/app.php
@@ -151,7 +151,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Dusk\DuskServiceProvider::class,
-
+        Sentry\SentryLaravel\SentryLaravelServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -204,6 +204,7 @@ return [
         'Response' => Illuminate\Support\Facades\Response::class,
         'Route' => Illuminate\Support\Facades\Route::class,
         'Schema' => Illuminate\Support\Facades\Schema::class,
+        'Sentry' => Sentry\SentryLaravel\SentryFacade::class,
         'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
         'URL' => Illuminate\Support\Facades\URL::class,
