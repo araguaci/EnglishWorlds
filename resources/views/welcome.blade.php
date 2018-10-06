1c6ff9875b1f3c52ab16446e6f931bfbf8bbc1c0
Salim Djerbouh <github@caddydz.me>
2018-10-06 17:58:36 +0100

    google analytics
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,20 +13,13 @@
     <meta name="keywords" content="english, dz, algeria, learn, social" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.14/semantic.min.css" integrity="sha256-FpjP45Pz019159CFCReBCsZtVeCvGawN2Om1w+SIi0A=" crossorigin="anonymous" />
     <link rel="stylesheet" type="text/css" href="css/home.css">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-117155922-6"></script>
     <script>
-        (function(i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function() {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-        ga('create', 'UA-44039803-2', 'auto');
-        ga('send', 'pageview');
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-117155922-6');
     </script>
 </head>
 
