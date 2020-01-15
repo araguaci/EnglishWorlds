5619c62d03a0c6475f3214ac7853453d5285dc4a
Salim Djerbouh <caddydz4@gmail.com>
2020-01-15 13:55:31 +0100

    Apply fixes from StyleCI (#182)
    [ci skip] [skip ci]

--- app/User.php
+++ app/User.php
@@ -11,6 +11,7 @@ class User extends Authenticatable implements ReacterableContract
 {
     use Notifiable;
     use Reacterable;
+
     public function shouldRegisterAsLoveReacterOnCreate(): bool
     {
         return true;
